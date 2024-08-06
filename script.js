d340839591169c51b631b05c49326b6e80ada168
Saso Trpovski <sasotrpovski@gmail.com>
2024-08-06 12:35:09 +0200

    new face2

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Smooth scroll
+    document.querySelectorAll('nav a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
